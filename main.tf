c14c2d03b1a1bd4328da8f86d4a9fe4783e3029b
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-03-24 12:48:03 +0530

    terraform learning

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "instance" {
+  count = 5
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0ce01673ed9893e29"]
+
+  tags = {
+    Name = "var.components[count.index]"
+  }
+}
+
+resource "aws_route53_record" "" {
+  zone_id = "Z02549774QMYGMZM7W06"
+  name    = "${var.components[count.index]}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[count.index].private_ip]
+}
+
+
+
+variable "components" {
+  default = ["frontend" , "posrgresql" , "auth-service", "portfolio-service" , "analytics"]
+}
